515abe8addb739a80d9918fcc79d2ae93cc3a5e0
max_ag <max.stribrny@gmail.com>
2025-03-22 20:46:44 +0100

    Laptop before merge

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -113,6 +113,7 @@
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
     pkgs.nushell
+    pkgs.wl-clipboard
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
